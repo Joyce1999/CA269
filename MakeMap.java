9c6d78055c19aa18858ca3fe9aba7476e7f620cd
Joyce1999 <45068001+Joyce1999@users.noreply.github.com>
2020-08-26 12:18:41 +0100

    Create MakeMap.java

---
+++ MakeMap.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class DemoMap {
+  public static void main(String [] args) {
+    Map<String, Integer> phoneNumbers = new HashMap<String, Integer>();
+    phoneNumbers.put("Jane", 0123456);
+    phoneNumbers.put("Zebidee", 666);
+    phoneNumbers.put("Johnny", 999);
+    
+    System.out.println("Zeb's number is " + phoneNumber.get("Zebidee");
